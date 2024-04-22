499b241dca3375559b23ade345a9b361f2aa70d5
Mei <Maitran.ttvh@gmail.com>
2024-04-22 15:03:53 +0800

    First modified HW2

--- src/hw2/HomeWork2.java
+++ src/hw2/HomeWork2.java
@@ -2,38 +2,53 @@ package hw2;
 
 public class HomeWork2 {
 	public static void main(String[] args) {
-////	計算1~1000的偶數和 (2+4+6+8+...+1000)
-//		int sum = 0;
-//		int i;
-//		for (i = 2; i <= 1000; i++) {
-//			if (i % 2 == 0)
-//				sum += i;
-//		}
-//		System.out.println("1~1000 的偶數和是：" + sum);
-//
-//// 計算1~10的連乘積 (1*2*3*...*10)(用for迴圈)			
-//		int result1 = 1;
-//		for (int x = 1; x <= 10; x++)
-//			result1 *= x;
-//		System.out.println("1~10的連乘積是：" + result1);
-//
-////計算1~10的連乘積 (1*2*3*...*10)(用for迴圈)
-//		int y = 1;
-//		int result2 = 1;
-//		while (y <= 10) {
-//			result2 *= y;
-//			y++;
-//		}
-//		System.out.println("1~10的連乘積是：" + result2);
-//
-//// 請設計一隻Java程式,輸出結果為以下:1 4 9 16 25 36 49 64 81 100		
-//		for (int a = 1; a <= 10; a++) {
-//			System.out.print((int) Math.pow(a, 2) + " ");
-//		}
-
-// 阿文很熱衷大樂透 (1 ~ 49),但他不喜歡有4的數字,不論是個位數或是十位數。請設計一隻程式,輸出結果為阿文可以選擇的數字有哪些?總共有幾個?		
-
+//	計算1~1000的偶數和 (2+4+6+8+...+1000)
+		int sum = 0;
+		int i;
+		for (i = 2; i <= 1000; i++) {
+			if (i % 2 == 0)
+				sum += i;
+		}
+		System.out.println("1~1000 的偶數和是：" + sum);
 		
+		System.out.println();
+// 計算1~10的連乘積 (1*2*3*...*10)(用for迴圈)			
+		int result1 = 1;
+		for (int x = 1; x <= 10; x++)
+			result1 *= x;
+		System.out.println("1~10的連乘積是：" + result1);
+
+		System.out.println();
+//計算1~10的連乘積 (1*2*3*...*10)(用for迴圈)
+		int y = 1;
+		int result2 = 1;
+		while (y <= 10) {
+			result2 *= y;
+			y++;
+		}
+		System.out.println("1~10的連乘積是：" + result2);
+
+		System.out.println();
+// 請設計一隻Java程式,輸出結果為以下:1 4 9 16 25 36 49 64 81 100		
+		for (int a = 1; a <= 10; a++) {
+			System.out.print((int) Math.pow(a, 2) + " ");
+		}
+
+		System.out.println();
+		System.out.println();
+// 阿文很熱衷大樂透 (1 ~ 49),但他不喜歡有4的數字,不論是個位數或是十位數。請設計一隻程式,輸出結果為阿文可以選擇的數字有哪些?總共有幾個?		
+		int number;
+		int count = 0;
+		for (number = 1; number <= 49; number++) {
+			if (number % 10 != 4 && number <= 39) {
+				System.out.print(number + " ");
+				count++;
+			}
+		}
+		System.out.print("\n 共有：" + count + "個");
+
+		System.out.println();
+		System.out.println();
 // 第 6 題
 		for (int e = 10; e >= 1; e--) {
 			for (int f = 1; f <= e; f++) {
@@ -41,5 +56,23 @@ public class HomeWork2 {
 			}
 			System.out.println();
 		}
+
+		System.out.println();
+// 請設計一隻JAVA程式，輸出結果為以下(使用迴圈)
+		/*
+		 A 
+		 BB 
+		 CCC 
+		 DDDD 
+		 EEEEE 
+		 FFFFFF
+		 */
+		for (char M = 65; M <= 70; M++) {
+			for (char N = 65; N <= M; N++)
+				System.out.print(M);
+			System.out.println();
+		}
+
 	}
+
 }
--- src/hw2/TestNineNine.java
+++ src/hw2/TestNineNine.java
@@ -3,36 +3,36 @@ package hw2;
 public class TestNineNine {
 	public static void main(String[] args) {
 		// for+while
-		int i=1,j=1;
-//		for (i = 1; i < 10; i++) {
-//			while (j < 10) {
-//				System.out.print(i + "x" + j + "=" + i * j);
-//				j++;
-//			}
-//			System.out.println();
-//		}
-		
-		
-		//for + do while
-//		for (i = 1; i<10; i++) {
-//			do {
-//				System.out.print(i + "x" + j + "=" + i * j);
-//				j++;
-//			} while (j<10);
-//			System.out.println();
-//			}
-		
-		
-		//while + do while
-		while (i<10) {
-			i++;
+		for (int i = 1; i < 10; i++) {
+			int j = 1;
+			while (j < 10) {
+				System.out.print("\t" + i + "x" + j + "=" + i * j);
+				j++;
+			}
+			System.out.println();
 		}
-		do {
-			j++;
-			System.out.print(i + "x" + j + "=" + i * j);
-		} while (j<10);
-			
+		System.out.println("*****************************************************");
+		// for + do while
+		for (int x = 1; x < 10; x++) {
+			int y = 1;
+			do {
+				System.out.print("\t" + x + "x" + y + "=" + x * y);
+				y++;
+			} while (y < 10);
+			System.out.println();
 		}
-		
-		
+		System.out.println("*****************************************************");
+
+		// while + do while
+		int a = 1;
+		while (a < 10) {
+			int b = 1;
+			do {
+				System.out.print("\t" + a + "x" + b + "=" + a * b);
+				b++;
+			} while (b < 10);
+			a++;
+			System.out.println();
+		}
 	}
+}
